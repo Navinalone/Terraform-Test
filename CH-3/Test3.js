759c83fe39024013cdb27a257d592de439b76850
Navin <124908644+Navinalone@users.noreply.github.com>
2023-02-14 21:35:07 +0530

    Update Test3.js

--- CH-3/Test3.js
+++ CH-3/Test3.js
@@ -1,14 +1,33 @@
 "use strict";
-var object = { 'a': { 'b': { 'c': 'Test-3 verified' }
+const obj = {
+    "prop": [
+        {
+            "key": "FOOD",
+            "value": "Food is wonderfull, food is great"
+        },
+        {
+            "key": "BAR",
+            "value": "Bar is bad, really bad"
+        },
+        {
+            "key": "BAR",
+            "value": "Bar is good, really good"
+        }
+    ]
+};
+const findByKey = (obj, key) => {
+    const arr = obj['prop'];
+    if (arr.length) {
+        const result = arr.filter((el) => {
+            return el['key'] === key;
+        });
+        // console.log(result);
+        if (result.length) {
+            return result[0].value;
+        }
+        else {
+            return 'no value found';
+        }
     }
 };
-var val = getValuefromObjectsUsingKeys(object, "a/b/c");
-function getValuefromObjectsUsingKeys(object1, key) {
-    let keys = key.split('/');
-    keys.forEach((x) => {
-        object1 = object1[x];
-    });
-    return object1;
-}
-let value = val ? val : null;
-console.log(value);
+console.log(findByKey(obj, 'FOOD'));
